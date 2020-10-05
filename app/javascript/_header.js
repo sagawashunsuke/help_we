70c977f91c3a5d49084f309616f22c66c81d7c9d
sagawashunsuke <shunkurin73@gmail.com>
2020-10-05 11:57:54 +0900

    途中

---
+++ app/javascript/_header.js
@@ -0,0 +1,16 @@
+// window.addEventListener('load', function(){
+
+//   const pullDownButton = document.getElementsByClassName("helpwe-icon")
+
+//   pullDownButton.addEventListener('mouseover', function(){
+//     console.log("mouseover OK")
+//   })
+
+//   pullDownButton.addEventListener('mouseout', function(){
+//     console.log("mouseout OK")
+//   })
+
+//   pullDownButton.addEventListener('click', function() {
+//     console.log("click OK")
+//   })
+// })
